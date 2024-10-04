16c158218e561fc7d8629370895001f00f8b28e0
Jon <152871391+jsmaxi@users.noreply.github.com>
2024-10-05 00:33:34 +0300

    X, SCF, BIGGER icons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +14,34 @@ export default function Home() {
           priority
         />
         <div className="max-w-md mx-auto text-center">
+          <Link href="https://x.com/sentinel_fi" target="_blank">
+            <Image
+              className="border-none"
+              src="/images/x.jpg"
+              alt="Social"
+              width={75}
+              height={75}
+            />
+          </Link>
+        </div>
+        <div className="max-w-md mx-auto text-center">
           <p className="text-3xl mb-8">Hedge Risks on Soroban</p>
           <p className="text-xl">Coming Soon...</p>
+        </div>
+        <br />
+        <div className="max-w-md mx-auto grid grid-cols-2 gap-4 content-center">
+          <Image
+            src="/images/SCFLogoWithName.svg"
+            alt="SCF"
+            width={125}
+            height={125}
+          />
+          <Image
+            src="/images/BIGGER.svg"
+            alt="BIGGER"
+            width={100}
+            height={100}
+          />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
